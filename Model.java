383b65b3f2a32ef297d0a685db68824e82fa1c38
Caio de Jesus <122991872+caiodjss@users.noreply.github.com>
2024-08-14 21:32:42 -0300

    Aula 6 Java

---
+++ Model.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author 013.397490
+ */
+public class Model {
+    private String nome;
+    private String email;
+    private String zap;
+    private String uf;
+    private String cidade;
+    private String sexo;
+    /**
+     * @param args the command line arguments
+     */
+
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the zap
+     */
+    public String getZap() {
+        return zap;
+    }
+
+    /**
+     * @param zap the zap to set
+     */
+    public void setZap(String zap) {
+        this.zap = zap;
+    }
+
+    /**
+     * @return the uf
+     */
+    public String getUf() {
+        return uf;
+    }
+
+    /**
+     * @param uf the uf to set
+     */
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+}
